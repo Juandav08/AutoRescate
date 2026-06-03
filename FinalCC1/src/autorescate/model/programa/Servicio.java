58b4fb5952cc5a4e8acdd2c0a2059d4c235bc7fd
Juandav08 <111824901+Juandav08@users.noreply.github.com>
2026-06-02 23:24:23 -0500

    Ajusta documentacion del enum Servicio

--- FinalCC1/src/autorescate/model/programa/Servicio.java
+++ FinalCC1/src/autorescate/model/programa/Servicio.java
@@ -1,15 +1,8 @@
 package autorescate.model.programa;
 
-/**
- * Tipos de servicio que puede prestar AutoRescate 24/7.
- */
 public enum Servicio {
-    /** Grua para remolque de vehiculos. */
     GRUA,
-    /** Moto de apoyo para asistencias rapidas. */
     MOTO_APOYO,
-    /** Camioneta equipada para asistencia en ruta. */
     CAMIONETA_ASISTENCIA,
-    /** Vehiculo liviano de respuesta rapida. */
     VEHICULO_LIVIANO
 }
